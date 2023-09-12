4f46989d3b8674c3f8ebc4a03f8c0dceea83aa7c
eriswaris <87966056+eriswaris@users.noreply.github.com>
2023-09-12 14:09:55 +0430

    Update 1_✍️_QA_Log_Update.py

--- 1_✍️_QA_Log_Update.py
+++ 1_✍️_QA_Log_Update.py
@@ -5,7 +5,6 @@ from os import write
 import streamlit as st
 import pandas as pd
 import numpy as np
-from google.oauth2.service_account import Credentials
 from io import BytesIO
 import xlsxwriter
 
