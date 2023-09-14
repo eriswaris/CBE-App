768dc9caacd3dd2b472ffb58dd8f6d25d9ec9285
eriswaris <87966056+eriswaris@users.noreply.github.com>
2023-09-14 06:01:52 +0430

    Home.py

--- Home.py
+++ Home.py
@@ -3,16 +3,55 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Multipage APP",
+    page_title="UNICEF-CBE",
     page_icon=("Boom")
 )
 st.subheader('UNICEF CBE Project - ACT & PPC', divider='rainbow')
 
-#text writer an
-st.markdown('<h1 class="animate__animated animate__fadeInDown">Hello!</h1>', unsafe_allow_html=True)
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
 
 
-st.markdown(
+    st.markdown('<h1 class="animate__animated animate__fadeInDown">Hello!</h1>', unsafe_allow_html=True)
+
+
+    st.markdown(
     """
     <style>
     .unicef-text {
@@ -24,7 +63,7 @@ st.markdown(
 )
 
 
-st.markdown(
+    st.markdown(
     """
     <style>
     @keyframes typing {
@@ -52,15 +91,17 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("""
+    st.markdown("""
     <h1 class="typewriter-text">The purpose of this webApp is to maintain high-quality data.</h1>
     """,
     unsafe_allow_html=True
 )
 
 
-multi = '''
+    multi = '''
  If you run into any technical problems or difficulties, feel free to contact Abdul Waris Amini, the Senior Data Officer at Premium Performance Consulting.
+'''
+    st.markdown(multi)
 '''
 st.markdown(multi)
 
