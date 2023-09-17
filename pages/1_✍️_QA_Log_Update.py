d391cdbc323c7dbdadd275ca377f6c610f574309
eriswaris <87966056+eriswaris@users.noreply.github.com>
2023-09-17 15:33:01 +0430

    1_✍️_QA_Log_Update.py

--- pages/1_✍️_QA_Log_Update.py
+++ pages/1_✍️_QA_Log_Update.py
@@ -108,16 +108,20 @@ def Update():
     st.write(Merge_datasets)
 
     # Load data from Google sheet
+    sheet_id = "1UeqKgO4T3Gy9MqfB8qHfDFAHVoX7XD9cz82UP5CIjBg"
+    sheet_name = "QA_Log"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+    QA_log = pd.read_csv(url)
+    QA_log = QA_log['KEY']
+
+    Merge_datasets = Merge_datasets[~Merge_datasets.KEY.isin(QA_log)]
+    st.subheader('Removing Duplicate KEY from the dataset')
+    st.write(Merge_datasets)
+
     gc = gspread.service_account(filename='waris.json')
     tab_name = 'QA_Log'
     sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1UeqKgO4T3Gy9MqfB8qHfDFAHVoX7XD9cz82UP5CIjBg/edit#gid=1946290')
-
-    sheet = pd.read_csv(sheet)
-    df_id = sheet['KEY']
-
-    Merge_datasets = Merge_datasets[~Merge_datasets.KEY.isin(df_id)]
-    st.subheader('Removing Duplicate KEY from the dataset')
-    st.write(Merge_datasets)
 
     sheet.values_append(tab_name, {'valueInputOption': 'USER_ENTERED'}, {'values': Merge_datasets.astype(str).values.tolist()})
     st.markdown('Updated!')
