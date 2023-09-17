025ba761cea53907400dddb3373d56faadbe7cf6
eriswaris <87966056+eriswaris@users.noreply.github.com>
2023-09-17 09:00:34 +0430

    1_✍️_QA_Log_Update.py

--- pages/1_✍️_QA_Log_Update.py
+++ pages/1_✍️_QA_Log_Update.py
@@ -97,8 +97,8 @@ def Update():
     Tool_6['Tool_Name'] = "Tool 6"
     Tool_6 = Tool_6[['KEY', 'Tool_Name', 'Province', 'District', 'Village', 'CBE_Name', 'CBE_Key', 'Surveyor_Name', 'Surveyor_Id']]
 
-    Tool_6['Tool_Name'] = "Tool 6"
-    Tool_6 = Tool_6[['KEY', 'Tool_Name', 'Province', 'District', 'Village', 'CBE_Name', 'CBE_Key', 'Surveyor_Name', 'Surveyor_Id']]
+    Tool_9['Tool_Name'] = "Tool 6"
+    Tool_9 = Tool_6[['KEY', 'Tool_Name', 'Province', 'District', 'Village', 'CBE_Name', 'CBE_Key', 'Surveyor_Name', 'Surveyor_Id']]
 
     Tool_8['Tool_Name'] = "Tool 8"
     Tool_8 = Tool_8[['KEY', 'Tool_Name', 'Province', 'District', 'Village_Town_Name', 'CBE_Name', 'CBE_Key', 'Surveyor_Name', 'Surveyor_Id']]
